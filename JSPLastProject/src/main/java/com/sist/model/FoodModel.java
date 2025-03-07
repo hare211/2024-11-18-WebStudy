2c414ffb36e46fbb61e14e8f9889cc181c0172c4
hare211 <blackhole2748@gmail.com>
2025-03-07 15:27:03 +0900

    ajax js 로 데이터 전송 모델(JSONObject)

--- JSPLastProject/src/main/java/com/sist/model/FoodModel.java
+++ JSPLastProject/src/main/java/com/sist/model/FoodModel.java
@@ -1,9 +1,13 @@
 package com.sist.model;
 
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.sist.controller.Controller;
 import com.sist.controller.RequestMapping;
@@ -94,5 +98,77 @@ public class FoodModel {
 		request.setAttribute("main_jsp", "../food/food_detail.jsp");
 		
 		return "../main/main.jsp";
+	}
+	@RequestMapping("food/food_find.do")
+	public String recipe_find(HttpServletRequest request, HttpServletResponse response) {
+		
+		request.setAttribute("main_jsp", "../food/food_find.jsp");
+		
+		return "../main/main.jsp";
+	}
+	@RequestMapping("food/food_find_ajax.do")
+	public void recipe_find_ajax(HttpServletRequest request, HttpServletResponse response) {
+		String page = request.getParameter("page");
+		String fd = request.getParameter("fd");
+		String ss = request.getParameter("ss");
+		
+		int curPage = Integer.parseInt(page);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		int start = (12 * curPage) - 11;
+		int end = 12 * curPage;
+		
+		map.put("start", start);
+		map.put("end", end);
+		map.put("fd", fd);
+		map.put("ss", ss);
+		
+		List<FoodVO> list = FoodDAO.foodFindData(map);
+		int totalPage = FoodDAO.foodFindTotalPage(map);
+		
+		final int BLOCK = 10;
+		int startPage = ((curPage - 1) / BLOCK * BLOCK) + 1;
+		int endPage = ((curPage - 1) / BLOCK * BLOCK) + BLOCK;
+		
+		if (endPage > totalPage) {
+			endPage = totalPage;
+		}
+		
+		// JSON 변경 (자바스크립트에서 사용 가능한 데이터 타입으로 변경)
+		JSONArray arr = new JSONArray();
+		int i = 0;
+		
+		for (FoodVO vo : list) {
+			JSONObject obj = new JSONObject();
+			obj.put("fno", vo.getFno());
+			obj.put("name", vo.getName());
+			obj.put("poster", vo.getPoster());
+			obj.put("score", vo.getScore());
+			obj.put("type", vo.getType());
+			obj.put("content", vo.getContent());
+			obj.put("theme", vo.getTheme());
+			obj.put("phone", vo.getPhone());
+			obj.put("address", vo.getAddress());
+			obj.put("likecount", vo.getLikecount());
+			obj.put("replycount", vo.getReplycount());
+			
+			if (i == 0) {
+				obj.put("curPage", curPage);
+				obj.put("totalPage", totalPage);
+				obj.put("startPage", startPage);
+				obj.put("endPage", endPage);
+			}
+			
+			arr.add(obj);
+			i++;
+		}
+		try {
+			response.setContentType("text/plain;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.write(arr.toJSONString());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
 	}
 }
